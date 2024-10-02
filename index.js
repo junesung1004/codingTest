af2caefd6fdd0ab4ab20a1caf57dbee9148f9fe2
junesung1004 <junecoding1004@gmail.com>
2024-10-02 17:29:41 +0900

    코테 입문 난이도 1페이지 완료

--- index.js
+++ index.js
@@ -192,3 +192,185 @@ strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수
 // };
 // let string = ["123", "12313123", "hello world"];
 // console.log(solution(string));
+
+/*
+16번
+정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, 
+array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// const solution = (array, n) => {
+//   let count = 0;
+//   for (let value of array) {
+//     if (value === n) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+// const solution = (array, n) => {
+//   const Array = array.filter((item) => item === n);
+//   const ArrayLength = Array.length;
+//   return ArrayLength;
+// };
+// let array = [1, 2, 3, 4, 5, 6, 7, 1, 5, 4];
+// let n = 1;
+// console.log(solution(array, n));
+
+/*
+17번
+어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 
+처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+*/
+
+// const solution = (n, t) => {
+//   const result = n * Math.pow(2, t);
+//   return result;
+// };
+
+// console.log(solution(5, 5));
+
+/*
+18번
+문자열 my_string이 매개변수로 주어집니다. 
+my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+// const solution = (my_string) => {
+//   //문자열을 일단 배열로 바꾼다. 문자열 자체를 뒤집는 메소드는 존재하지 않음
+//   //split메소드를 사용하여 split("")를 기준으로 배열로 변환
+//   const strarr = my_string.split("");
+
+//   //문자열을 배열로 변환 후 배열 뒤집는 reverse()메소드를 사용하여 뒤집는다.
+//   const reverseStrArr = strarr.reverse();
+
+//   //문자열을 배열로 변환 후 뒤집은 후에 배열을 다시 문자열로 합치려면 join("") 메소드를 사용
+//   const string = reverseStrArr.join("");
+//   return string;
+// };
+
+// console.log(solution("Hello"));
+
+/*
+19번
+정수가 담긴 리스트 num_list가 주어질 때, 
+num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// const solution = (num_list) => {
+//   let box1 = [];
+//   let box2 = [];
+//   num_list.map((item) => {
+//     if (item % 2 === 0) {
+//       box1.push(item);
+//     } else {
+//       box2.push(item);
+//     }
+//   });
+//   let answer = [box1.length, box2.length];
+//   return answer;
+// };
+
+// console.log(solution([2, 4, 2, 1, 2, 4, 6, 8, 12, 10, 11]));
+
+/*
+20번
+머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 
+양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 
+정수 n과 k가 매개변수로 주어졌을 때, 
+양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// const solution = (n, k) => {
+//   let 양꼬치가격 = 12000;
+//   let 음료수가격 = 2000;
+//   if (n % 10 === 0) {
+//     k++;
+//   }
+//   let 양꼬치 = n * 양꼬치가격;
+//   let 음료수 = k * 음료수가격;
+
+//   let result = 양꼬치 + 음료수;
+//   return result;
+// };
+// console.log(solution(12, 2));
+
+/*
+21번
+머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 
+머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 
+머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// const solution = (array, height) => {
+//   let count = 0;
+//   for (i of array) {
+//     if (height > i) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+// const array = [152, 164, 155, 182, 165];
+// console.log(solution(array, 161));
+
+/*
+22번
+정수 배열 numbers가 매개변수로 주어집니다. 
+numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+// const solution = (numbers) => {
+//   let answer = numbers.reduce((acc, cur) => [...acc, cur * 2], []);
+//   return answer;
+// };
+// console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+/*
+23번
+중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 
+예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 
+정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+*/
+
+// const solution = (array) => {
+//   let arraySort = array.sort((a, b) => a - b);
+//   let middle = Math.floor(arraySort.length / 2);
+//   let answer = array[middle];
+//   return answer;
+// };
+// console.log(solution([1, 2, 7, 10, 11]));
+
+/*
+24번
+정수 n이 매개변수로 주어질 때,
+n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+// const solution = (n) => {
+//   let arr = [];
+//   for (let i = 0; i <= n; i++) {
+//     if (i % 2 === 1) {
+//       arr.push(i);
+//     }
+//   }
+//   let sortArr = arr.sort((a, b) => a - b);
+//   return sortArr;
+// };
+// console.log(solution(11));
+
+/*
+25번
+머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 
+피자를 나눠먹을 사람의 수 n이 주어질 때, 
+모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+n	result
+7	  1
+1	  1
+15	3
+*/
+
+// const solution = (n) => {
+//   return Math.ceil(n / 7);
+// };
+// console.log(solution(20));
